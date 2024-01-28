dfbec4aac50920150579e5a7f077cdb4335bb067
TheIronFortress <K_Brooksby@hotmail.com>
2024-01-28 14:01:02 -0700

    Damage Integration Start

---
+++ Assets/Scripts/EnemyAI/Projectile_Ranger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile_Ranger : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifetime;
+    [System.NonSerialized] public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        rb.velocityX = speed;
+
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Player":
+                collision.gameObject.GetComponent<Player>().TakeDamage(damage);
+                Destroy(gameObject);
+                break;
+            case "Wall":
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
